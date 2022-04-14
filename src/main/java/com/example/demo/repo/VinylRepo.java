a3669fd7a9e911b70e82d25720774dd70ade0f0a
owencornock <owen52new@gmail.com>
2022-04-14 12:07:04 +0100

    PROJ-49 #comment Making inteface and completing interface

--- src/main/java/com/example/demo/repo/VinylRepo.java
+++ src/main/java/com/example/demo/repo/VinylRepo.java
@@ -1,5 +1,18 @@
 package com.example.demo.repo;
 
-public class VinylRepo {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.Vinyl;
+
+@Repository
+public interface VinylRepo extends JpaRepository<Vinyl, Integer> {
+
+	List<Vinyl> findByArtistIgnoreCase(String artist);
+	
+	List<Vinyl> findByAlbumIgnoreCase(String album);
+	
+	List<Vinyl> findBySize(Integer size);
 }
