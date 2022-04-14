06ea7157ec124a8aaa864dc5d3c69d2d13dc3aa7
owencornock <owen52new@gmail.com>
2022-04-14 18:02:59 +0100

    PROJ-6 #comment Completing functionality for front end, inlcuding find by album, artist and vinyl size

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -55,6 +55,80 @@ const get = () => {
         console.log(err);
       });
   }
+  const put = (id) => {
+      axios.put(`http://localhost:8080/replace/${id}`, {
+          id: DOM.inputUpdateId.value,
+          artist: DOM.inputUpdateArtist.value,
+          album: DOM.inputUpdateAlbum.value,
+          size: DOM.inputUpdateSize.value
+      })
+      .then((response) => {
+          console.log(response);
+      }).catch((err) => {
+          console.log(err);
+      });
+  }
+  const deleteId = (id) => {
+      axios.delete(`http://localhost:8080/remove/${id}`, { id: DOM.inputDeleteId.value })
+      .then((response) => {
+          console.log(response);
+      }).catch((err) => {
+          console.log(err);
+      });
+  }
+  const getArtist = (artist) => {
+    DOM.artistOutput.innerHTML = ``;
+  
+    axios.get(`http://localhost:8080/getByArtist/${artist}`)
+      .then((response) => {
+        if (!Array.isArray(response.data)) {
+          writeItem(response.data);
+        } else {
+          for (let item of response.data) {
+            writeItem(item);
+          }
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+  const getAlbum = (album) => {
+    DOM.albumOutput.innerHTML = ``;
+  
+    axios.get(`http://localhost:8080/getByAlbum/${album}`)
+      .then((response) => {
+        if (!Array.isArray(response.data)) {
+          writeItem(response.data);
+        } else {
+          for (let item of response.data) {
+            writeItem(item);
+          }
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+  const getSize = (size) => {
+    DOM.sizeOutput.innerHTML = ``;
+  
+    axios.get(`http://localhost:8080/getBySize/${size}`)
+      .then((response) => {
+        if (!Array.isArray(response.data)) {
+          writeItem(response.data);
+        } else {
+          for (let item of response.data) {
+            writeItem(item);
+          }
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
   DOM.buttonReadAll.onclick = () => get();
   DOM.buttonCreate.onclick = () => post();
   DOM.buttonRead.onclick = () => getNew(DOM.inputId.value);
+  DOM.buttonUpdate.onclick = () => put(DOM.inputUpdateId.value);
+  DOM.buttonDelete.onclick = () => deleteId(DOM.inputDeleteId.value);
+  DOM.buttonArtist.onclick = () => getArtist(DOM.inputFindArtist.value);
+  DOM.buttonAlbum.onclick = () => getAlbum(DOM.inputFindAlbum.value);
+  DOM.buttonSize.onclick = () => getSize(DOM.inputFindSize.value);
